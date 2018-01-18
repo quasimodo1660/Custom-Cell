50b7d0155f3be3b4e9f6e82703e64d2b8e82fd19
Quasimodo <quasimodo1660@gmail.com>
2018-01-17 16:14:39 -0800

    Custom Cell Demo

---
+++ CustomCell/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  CustomCell
+//
+//  Created by Peisure on 1/17/18.
+//  Copyright © 2018 Ben. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var rightLabel: UILabel!    
+    @IBOutlet weak var leftButton: UIButton!
+}
